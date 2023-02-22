54a05dc92008f73943b454b7df8f54cb7e76f3b0
Heriberto Robles Coronado <rrobleshc@hotmail.com>
2023-02-21 17:04:20 -0700

    API

---
+++ Sales.API/Migrations/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Sales.Shared.Entities;
+
+namespace Sales.API.Migrations
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<Country> Countries { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Country>().HasIndex(x => x.Name) .IsUnique();
+        }
+    }
+}
