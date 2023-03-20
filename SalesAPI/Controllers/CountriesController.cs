eeb2e4b5b6c4194587d1e880c0647e546cefadd7
Heriberto Robles Coronado <rrobleshc@hotmail.com>
2023-03-20 15:44:25 -0700

    Add Put, Delete and Get

--- SalesAPI/Controllers/CountriesController.cs
+++ SalesAPI/Controllers/CountriesController.cs
@@ -24,6 +24,17 @@ namespace SalesAPI.Controllers
             return Ok(await _context.Countries.ToListAsync());
         }
 
+        [HttpGet("{id:int}")]  
+        public async Task<IActionResult> GetAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null) 
+            {
+                return NotFound();  
+            }
+            return Ok(country);
+        }
+        
         [HttpPost]
         public async Task<ActionResult> PostAsync(Country country)
         {
@@ -31,6 +42,28 @@ namespace SalesAPI.Controllers
             await _context.SaveChangesAsync();  
             return Ok(country);    
 
+        }
+
+        [HttpPut]
+        public async Task<ActionResult> PutAsync(Country country)
+        {
+            _context.Update(country);
+            await _context.SaveChangesAsync();
+            return Ok(country);
+
+        }
+
+        [HttpDelete("{id:int}")]
+        public async Task<IActionResult> DeleteAsync(int id)
+        {
+            var country = await _context.Countries.FirstOrDefaultAsync(x => x.Id == id);
+            if (country == null)
+            {
+                return NotFound();
+            }
+            _context.Remove(country);
+            await _context.SaveChangesAsync();
+            return NoContent();
         }
 
     }
